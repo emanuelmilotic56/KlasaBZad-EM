73e70a388ca63380f567880f4a311b06cbf0a327
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-12-10 20:22:29 +0100

    Add files via upload

---
+++ KlasaBZad-EM/KlasaB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaBZad_EM
+{
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+
+    }
+}
---
+++ KlasaBZad-EM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaBZad_EM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string s = Console.ReadLine();
+            KlasaB x = new KlasaB();
+            if (s.Length > 2)
+                s = x.BezPrvogIZadnjeg(s);
+            Console.WriteLine(s);
+
+        }
+    }
+}
